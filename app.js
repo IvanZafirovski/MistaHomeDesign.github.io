ba1bbcb47c5cb216f098e8e826a14721e050cad1
Ivan Zafirovski <ivan_zafirovski@yahoo.com>
2023-03-05 12:10:15 +0100

    new update

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 "use strict";
 
 const showLearnMore = document.querySelector(".learn-more-button");
-const paragraphLM = document.querySelector(".test");
+const expirience = document.querySelector(".expirience");
+const overlay = document.querySelector(".overlay");
+const closeLM = document.querySelector(".close-learn-more");
+const title = document.querySelector(".title");
+const subtitle = document.querySelector(".subtitle");
 
 showLearnMore.addEventListener("click", function () {
-  paragraphLM.classList.remove("hidden");
+  console.log("learn more");
+  expirience.classList.remove("hidden");
+  title.
+
+});
+
+closeLM.addEventListener("click", function () {
+  expirience.classList.add("hidden");
+  
 });
